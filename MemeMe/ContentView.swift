5b89e61b22953b1d0a8176c2ab1399d3be8171ba
Bojan Belic <belic_bojan@sigmadelta.be>
2020-11-23 16:09:05 +0100

    Initial Commit

---
+++ MemeMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemeMe
+//
+//  Created by Bojan Belic on 23/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemeMe/MemeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeMeApp.swift
+//  MemeMe
+//
+//  Created by Bojan Belic on 23/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct MemeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
